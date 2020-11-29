c76816887c086977b335d41b403051ee0de5202c
yangyongxing <13540954048@163.com>
2020-11-29 22:38:50 +0800

    提交测试

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestController {
 
+    @ResponseBody
+    @RequestMapping("/findAll")
+    public String findAll(){
+        return null;
+    }
 }
